c9d09bf2a4162eb3bcd40ee2807d0ac62760f16e
swethakeerthipati <swethakeerthipati@gmail.com>
2023-06-26 01:00:07 +0200

    Modify Form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Form from "./components/Form";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const [activities, setActivities] = useState([]);
+  function handleAddActitvity(newActivity) {
+    // setActivities([...activities, newActivity]);
+    const updatedActivities = [...activities, { ...newActivity, id: uuidv4() }];
+    setActivities(updatedActivities);
+  }
+
   return (
     <div>
-      <Form />
+      <Form handleAddActitvity={handleAddActitvity} />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,36 @@
 import React from "react";
+
 import "../styles/Form.css";
 
-export default function Form() {
+export default function Form({ handleAddActivity }) {
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formdata = {
+      name: e.target.newActivity.value,
+      // console.log(name);
+      isGoodWeather: e.target.typeofWeather.checked,
+      // console.log(isGoodWeather);
+    };
+
+    //Resetting the form and focusing on the new activity input
+    e.target.reset();
+    e.target.newActivity.focus();
+    console.log(formdata);
+    // return formdata;
+    return handleAddActivity;
+  }
   return (
-    <form className="form">
+    <form onSubmit={handleSubmit} className="form">
       <h1>Weather App</h1>
       <label htmlFor="newActivity" className="label-input">
         Add a New Activity
       </label>
-      <input type="text" id="newActivity" className="text-input" />
+      <input
+        type="text"
+        id="newActivity"
+        className="textInput"
+        name="newActivity"
+      />
       <div className="checkbox">
         <input type="checkbox" name="typeofWeather" />
         <label htmlFor="good">Good Weather Activity</label>
