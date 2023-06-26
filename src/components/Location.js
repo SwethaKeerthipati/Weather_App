5f226b5d942f6f015e6f6871cd9da052a8b95068
Swetha Keerthipati <125989556+SwethaKeerthipati@users.noreply.github.com>
2023-06-26 14:18:26 +0200

    Update Location.js

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,1 +1,15 @@
+ function Location({location , setLocation}){
+    return(
+        <div className="container">
+        <label htmlFor="location">Choose the Location:</label>
+        <select id="location" value={location} onChange={(e) => setLocation(e.target.value)} name="location">
+            <option value="europe">Europe</option>
+            <option value="arctic">Arctic</option>
+            <option value="sahara">Sahara</option>
+            <option value="rainforest">Rainforest</option>
+        </select>
+        </div>
+    )
+}
 
+export default Location;
